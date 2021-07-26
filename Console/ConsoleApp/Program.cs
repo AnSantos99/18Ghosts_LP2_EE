66259a4150edd98d45f47277dd4f98dc7e60884b
AnSantos99 <an.santos95@hotmail.com>
2021-07-26 20:52:31 +0100

    Drawing board

--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            const int x_Dim = 5;
+            const int y_Dim = 5;
+
+            for (int i = 0; i < x_Dim; i++)
+            {
+                for (int j = 0; j < y_Dim; j++)
+                {
+                    Console.Write("-");
+                }
+
+                Console.WriteLine("  |");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            
+
         }
     }
 }
