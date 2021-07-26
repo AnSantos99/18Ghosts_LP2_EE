ae29137d8b3a3ce9b57f1e4f3d91b2270819d3e9
AnSantos99 <an.santos95@hotmail.com>
2021-07-26 20:56:49 +0100

    Add class game

---
+++ Console/ConsoleApp/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Game
+    {
+
+
+
+
+        public void StartGame() 
+        {
+            const int x_Dim = 5;
+            const int y_Dim = 5;
+
+            for (int i = 0; i < x_Dim; i++)
+            {
+                for (int j = 0; j < y_Dim; j++)
+                {
+                    Console.Write("-");
+                }
+
+                Console.WriteLine("  |");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -6,25 +6,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-
-            const int x_Dim = 5;
-            const int y_Dim = 5;
-
-            for (int i = 0; i < x_Dim; i++)
-            {
-                for (int j = 0; j < y_Dim; j++)
-                {
-                    Console.Write("-");
-                }
-
-                Console.WriteLine("  |");
-
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-            
-
+            Game start = new Game();
+            start.StartGame();
         }
     }
 }
