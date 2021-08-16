fd7520202131464a9eea3fa6221dce8dca8a22a1
AnSantos99 <an.santos95@hotmail.com>
2021-08-16 23:39:55 +0100

    Draw board

--- Console/ConsoleApp/Program.cs
+++ Console/ConsoleApp/Program.cs
@@ -17,11 +17,11 @@ namespace ConsoleApp
             {
                 for (int k = 0; k < 1; k++)
                 {
-                    Console.Write(" ╦");
+                    Console.Write("═══╦");
                 }
-                Console.Write(" ═════");
+                Console.Write("═════");
             }
-            Console.Write(" ╦");
+            Console.Write("═╦");
             Console.WriteLine();
             ///////////////////////////////////////////////////////////////////
 
@@ -29,7 +29,7 @@ namespace ConsoleApp
             // Drawing inside of board
             for (int x = 0; x < 5; x++)
             {
-                Console.Write($"{x}║");
+                Console.Write($" {x} ║");
 
                 for (int y = 0; y < 5; y++)
                 {
@@ -37,34 +37,52 @@ namespace ConsoleApp
                     
                     for (int i = 0; i < 1; i++)
                     {
-                        Console.Write("  │");
-                    }
+                        Console.Write("   │");
+                    }  
+                }
+
+                for (int m = 0; m < 1; m++)
+                {
+                    Console.Write("      ║");
                 }
                 Console.WriteLine();
+                
 
                 for (int a = 0; a < 1; a++)
                 {
-                    Console.Write(" ║");
+                    Console.Write("   ║");
                 }
 
                 for (int y = 0; y < 5; y++)
                 {
-                    
-                    Console.Write("───────");
+                    Console.Write("────────");
 
                     for (int i = 0; i < 1; i++)
                     {
                         Console.Write("┼");
                     }
                 }
-                //for (int j = 0; j < 1; j++)
-                //{
-                //    Console.Write("     ║");
-                //}
-                Console.WriteLine();
 
-                
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.Write("      ║");
+                }
+                Console.WriteLine();
             }
+
+            Console.Write("   ║");
+
+            for (int s = 0; s < 5; s++)
+            {
+                Console.Write($"   {s}   ");
+                for (int i = 0; i < 1; i++)
+                {
+                    Console.Write(" │");
+                }
+            }
+            Console.Write("      ║");
+
+            Console.WriteLine();
 
             ///////////////////////////////////////////////////////////////////
             // Drawing bottom part
@@ -72,15 +90,12 @@ namespace ConsoleApp
             {
                 for (int k = 0; k < 1; k++)
                 {
-                    Console.Write(" ╩");
+                    Console.Write("═══╩");
                 }
-                Console.Write(" ═════");
+                Console.Write("═════");
             }
-            Console.Write("╩");
-            Console.WriteLine();
-
-
-            Console.ReadKey();
+            Console.Write("═╩");
+            Console.WriteLine();  
         }
     }
 }
