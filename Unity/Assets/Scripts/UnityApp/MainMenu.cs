829346e7f533a4380593c3e1355853d16337dacd
AnSantos99 <an.santos95@hotmail.com>
2021-08-24 15:51:36 +0100

    Unity Main Menu set up

---
+++ Unity/Assets/Scripts/UnityApp/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the actual game scene
+    /// </summary>
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Instructions() 
+    {
+
+    }
+
+    /// <summary>
+    /// Quit the application
+    /// </summary>
+    public void QuitGame()
+    {
+        // For Testing
+        //UnityEditor.EditorApplication.isPlaying = false;
+
+        Application.Quit();
+    }
+}
