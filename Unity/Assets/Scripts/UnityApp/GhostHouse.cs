d74ff9d3c6eef5271a164b165cbea16c0530c47a
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2021-08-27 21:41:06 +0100

    Unity Ghost dragdrop working

--- Unity/Assets/Scripts/UnityApp/GhostHouse.cs
+++ Unity/Assets/Scripts/UnityApp/GhostHouse.cs
@@ -7,12 +7,44 @@ public class GhostHouse : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log("OnDrop");
-        if (eventData.pointerDrag != null)
+        if (this.gameObject.tag == "House")
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-            eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
-            eventData.pointerDrag.GetComponent<DragDrop>().defaultPos = transform.position;
+            Debug.Log("OnDrop");
+            Debug.Log(this.gameObject.tag);
+
+            if (eventData.pointerDrag != null)
+            {
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+               //if droped somewhere out of bounds, tp to previous pos
+                eventData.pointerDrag.GetComponent<DragDrop>().defaultPos = transform.position;
+            }
+        }
+
+        if (this.gameObject.tag == "Mirror")
+        {
+            if (eventData.pointerDrag != null)
+            {
+                Debug.Log(this.gameObject.tag);
+
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+                //if droped somewhere out of bounds, tp to previous pos
+                eventData.pointerDrag.GetComponent<DragDrop>().defaultPos = transform.position;
+            }
+        }
+        
+        if (this.gameObject.tag == "Portal")
+        {
+            if (eventData.pointerDrag != null)
+            {
+                Debug.Log(this.gameObject.tag);
+
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+                //if droped somewhere out of bounds, tp to previous pos
+                eventData.pointerDrag.GetComponent<DragDrop>().defaultPos = transform.position;
+            }
         }
     }
 }
