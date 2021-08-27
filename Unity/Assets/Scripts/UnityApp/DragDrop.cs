81c759f345ff59ab3c2406e78a57014425bd1570
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2021-08-27 18:13:01 +0100

    Add DragDrop and GhostHouse scripts

--- Unity/Assets/Scripts/UnityApp/DragDrop.cs
+++ Unity/Assets/Scripts/UnityApp/DragDrop.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Canvas canvas;
+    private CanvasGroup canvasGroup;
+
+    private RectTransform rectTransform;
+    public Vector3 defaultPos;
+    public bool droppedOnSlot;
+
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        defaultPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        defaultPos = transform.position;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeingDraged");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+        eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDraged");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        if (droppedOnSlot == false)
+        {
+            transform.position = defaultPos;
+        }
+
+    }
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+        Debug.Log("OnDrop Item");
     }
 }
---
+++ Unity/Assets/Scripts/UnityApp/GhostHouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GhostHouse : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<DragDrop>().droppedOnSlot = true;
+            eventData.pointerDrag.GetComponent<DragDrop>().defaultPos = transform.position;
+        }
+    }
+}
