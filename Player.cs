bc867650bddb192ab7840f2920a6cc308a562490
AnSantos99 <an.santos95@hotmail.com>
2021-08-28 01:40:21 +0100

    Add player enum to common proj

---
+++ Player.cs
@@ -0,0 +1,12 @@
+﻿namespace _18Ghosts
+{
+    /// <summary>
+    /// Player enum to change player turn
+    /// </summary>
+    public enum Player
+    {
+        Player1,
+        Player2,
+        none
+    }
+}
